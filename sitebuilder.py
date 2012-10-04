13a63796fb4ea2526a19aa424605842b99e36228
Yoav Ram <yoavram+bitbucket@gmail.com>
2012-10-04 10:24:49 +0200

    removed pygments.css

--- sitebuilder.py
+++ sitebuilder.py
@@ -48,10 +48,6 @@ def tag(tag):
     tagged = [p for p in pages if tag in p.meta.get('tags', [])]
     return render_template('tag.html', pages=tagged, tag=tag)
 
-@app.route('/pygments.css')
-def pygments_css():
-    return pygments_style_defs('tango'), 200, {'Content-Type': 'text/css'}
-
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == "build":
         freezer.freeze()
